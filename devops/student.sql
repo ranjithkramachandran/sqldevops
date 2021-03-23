8677565aa6a78e9537d817cb1685838a509c412d
Ranjith <ranjith.aws@outlook.com>
2021-03-23 22:57:27 +0000

    Added student dataof birth column

--- devops/student.sql
+++ devops/student.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[student]
 (
 	[RollNo] INT NOT NULL PRIMARY KEY Identity(1,1), 
-    [StudenName] NVARCHAR(50) NULL
+    [StudenName] NVARCHAR(50) NULL,
+	[DateOfBirth] Datetime
 )
